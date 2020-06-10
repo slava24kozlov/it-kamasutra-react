4ac996b1702f257c39df92b90226c0ebbcd4692f
Slava Kozlov <slava24kozlov@gmail.com>
2020-06-11 02:16:11 +0300

    lesson nine

---
+++ src/Headers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Headers = () => {
+  return (
+    <div className="headers">
+        <p className="headers-text">Home News Feed Messages</p>
+    </div>
+  );
+}
+
+export default Headers;
