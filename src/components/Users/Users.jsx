bed88c2b417081aebd4f6853a51367164b1aff26
Vyacheslav <slava24kozlov@gmail.com>
2021-06-25 02:42:09 +0300

    users page class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,64 +3,65 @@ import style from './Users.module.css'
 import axios from 'axios'
 import photoUser from '../../image/photoUser.png'
 
-const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUsers(response.data.items)
-            }
-        )
+class Users extends React.Component {
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => this.props.setUsers(response.data.items))
+        }
     }
-    return <div>
-        {props.users.map(el =>
-            <div key={el.id} className={style.main}>
-                <section className={style.userHead}>
-                    <div>
-                        <img src={el.photos.small != null ? el.photos.small : photoUser} alt='image avatar'/>
-                    </div>
-                    <div>
-                        {el.followed
-                            ? <button onClick={() => props.unfollow(el.id)}>UNFOLLOW</button>
-                            : <button onClick={() => props.follow(el.id)}>FOLLOW</button>}
-                    </div>
-                </section>
-                <section className={style.userInformation}>
+
+    render() {
+        return <div>
+            {this.props.users.map(el =>
+                <div key={el.id} className={style.main}>
+                    <section className={style.userHead}>
+                        <div>
+                            <img src={el.photos.small != null ? el.photos.small : photoUser} alt='image avatar'/>
+                        </div>
+                        <div>
+                            {el.followed
+                                ? <button onClick={() => this.props.unfollow(el.id)}>UNFOLLOW</button>
+                                : <button onClick={() => this.props.follow(el.id)}>FOLLOW</button>}
+                        </div>
+                    </section>
+                    <section className={style.userInformation}>
                     <span>
                         <div><b>{el.name}</b></div>
                         <div>{el.status ?? <i>No status</i>}</div>
                     </span>
-                </section>
-            </div>
-        )}
-    </div>
+                    </section>
+                </div>)}
+        </div>
+    }
 }
 
 export default Users;
 
 
 /*if (props.users.length === 0) {
-       props.setUsers([
-           {
-               id: 1,
-               followed: true,
-               imageUser: 'http://mhs.edu.af/en/wp-content/uploads/2015/05/unknown-user.png',
-               name: 'Dwayne J',
-               status: 'Our life is what our thoughts make it',
-               location: {country: 'Hayward', city: 'United States'}
-           },
-           {
-               id: 2,
-               followed: false,
-               imageUser: 'http://mhs.edu.af/en/wp-content/uploads/2015/05/unknown-user.png',
-               name: 'Tom H',
-               status: 'In the middle of difficulty lies opportunity',
-               location: {country: 'Kingston upon Thames', city: 'United Kingdom'}
-           },
-           {
-               id: 3,
-               followed: true,
-               imageUser: 'http://mhs.edu.af/en/wp-content/uploads/2015/05/unknown-user.png',
-               name: 'Jim C',
-               status: 'Life is a series of choices',
-               location: {country: 'Newmarket', city: 'Canada'}
-           }])
-   }*/
+   props.setUsers([
+       {
+           id: 1,
+           followed: true,
+           imageUser: 'http://mhs.edu.af/en/wp-content/uploads/2015/05/unknown-user.png',
+           name: 'Dwayne J',
+           status: 'Our life is what our thoughts make it',
+           location: {country: 'Hayward', city: 'United States'}
+       },
+       {
+           id: 2,
+           followed: false,
+           imageUser: 'http://mhs.edu.af/en/wp-content/uploads/2015/05/unknown-user.png',
+           name: 'Tom H',
+           status: 'In the middle of difficulty lies opportunity',
+           location: {country: 'Kingston upon Thames', city: 'United Kingdom'}
+       },
+       {
+           id: 3,
+           followed: true,
+           imageUser: 'http://mhs.edu.af/en/wp-content/uploads/2015/05/unknown-user.png',
+           name: 'Jim C',
+           status: 'Life is a series of choices',
+           location: {country: 'Newmarket', city: 'Canada'}
+       }])
+}*/
