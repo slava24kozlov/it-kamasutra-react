ab1e830d58c668dae8bb287eea5d1598d7ebf9b0
Slava Kozlov <v.kozlov@freshlimesoft.com>
2022-05-22 00:58:31 +0300

    added memorization a location after reload using the sessionStorage

--- src/components/MainContentContainer.tsx
+++ src/components/MainContentContainer.tsx
@@ -1,11 +1,12 @@
-import React, {lazy, Suspense } from "react";
-import {Redirect, Route} from "react-router-dom";
+import React, {lazy, Suspense} from "react";
+import {Redirect, Route, useLocation} from "react-router-dom";
 import {compose} from "redux";
 import {getAuthId, getIsAuth} from "../redux/selectors/AuthSelectors";
 import {connect, ConnectedProps} from "react-redux";
 import {withAuthRedirect} from "../hoc/AuthRedirect";
 import {AppStateType} from "../redux/store";
 import Preloader from "./common/Preloader/Preloader";
+
 const ProfileContainer = lazy(() => import("./Profile/ProfileContainer"))
 const FriendsContainer = lazy(() => import("./Friends/FriendsContainer"))
 const MessagesContainer = lazy(() => import("./Messages/MessagesContainer"))
@@ -13,25 +14,34 @@ const UsersContainer = lazy(() => import("./Users/UsersContainer"))
 const Communities = lazy(() => import("./Communities/Communities"))
 const Music = lazy(() => import("./Music/Music"))
 
-const MainContent = (props: PropsFromRedux) => (
-  <Suspense fallback={<Preloader/>}>
-    <Route path='/profile/:userId?' render={() =>
-      <ProfileContainer/>}/>
-    <Route path='/friends' render={() =>
-      <FriendsContainer/>}/>
-    <Route path='/messages' render={() =>
-      <MessagesContainer/>}/>
-    <Route path='/users' render={() =>
-      <UsersContainer/>}/>
-    <Route path='/communities' component={Communities}/>
-    <Route path='/music' component={Music}/>
-    <Redirect from='/' to={`/profile/${props.idAuthUser}`}/>
-  </Suspense>
-)
+const MainContent = (props: PropsFromRedux) => {
+    const currentURL = useLocation().pathname
+    if (currentURL !== sessionStorage.getItem("currentURL") && currentURL !== "/login") {
+        sessionStorage.setItem("currentURL", currentURL)
+    }
+
+    return (
+        <Suspense fallback={<Preloader/>}>
+            <Route path='/profile/:userId?' render={() =>
+                <ProfileContainer/>}/>
+            <Route path='/friends' render={() =>
+                <FriendsContainer/>}/>
+            <Route path='/messages' render={() =>
+                <MessagesContainer/>}/>
+            <Route path='/users' render={() =>
+                <UsersContainer/>}/>
+            <Route path='/communities' component={Communities}/>
+            <Route path='/music' component={Music}/>
+            <Redirect from='/'
+                      to={sessionStorage.getItem("currentURL") as string | null ? sessionStorage.getItem("currentURL") as string : `/profile/${props.idAuthUser}`}/>
+        </Suspense>
+    )
+
+}
 
 const mapStateToProps = (state: AppStateType) => ({
-  isAuthUser: getIsAuth(state),
-  idAuthUser: getAuthId(state),
+    isAuthUser: getIsAuth(state),
+    idAuthUser: getAuthId(state),
 })
 
 const connector = connect(mapStateToProps)
