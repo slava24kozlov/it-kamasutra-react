4150664d88547740ef29b3f2f1fede3fef172177
Slava Kozlov <v.kozlov@freshlimesoft.com>
2022-06-01 00:04:45 +0300

    fixed active links in Header

--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -4,20 +4,22 @@ import {connect} from "react-redux";
 import {getFriends} from "../../redux/selectors/SidebarSelectors";
 import Wrapper from "../common/Wrappers/WrapperComponents";
 
+console.log(matchMedia("screen and (min-width: 768px)"));
+
 const Friends = ({friendsBar}) => (
-  <Wrapper title="FRIENDS" className={style.main}>
-    <div className={style.main}>
-      {friendsBar.map(({name, image}) =>
-        <div key={name}>
-          <img src={image} alt={name} width="50" height="50"/>
-          <b>{name}</b>
-        </div>)}
-    </div>
-  </Wrapper>
+    <Wrapper title="FRIENDS" className={style.main}>
+        <div className={style.main}>
+            {matchMedia("screen and (min-width: 768px)").matches && friendsBar.map(({name, image}) =>
+                <div key={name}>
+                    <img src={image} alt={name} width="50" height="50"/>
+                    <b>{name}</b>
+                </div>)}
+        </div>
+    </Wrapper>
 );
 
 const mapStateToProps = (state) => ({
-  friendsBar: getFriends(state),
+    friendsBar: getFriends(state),
 });
 
 export default connect(mapStateToProps)(Friends);
