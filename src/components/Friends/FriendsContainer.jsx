001935d2a5553f1ed79461f187e1dfb09e9e45c2
Slava Kozlov <v.kozlov@freshlimesoft.com>
2022-06-25 16:45:13 +0300

    added the matchMedia and the sass:math module in css for the Friend component

--- src/components/Friends/FriendsContainer.jsx
+++ src/components/Friends/FriendsContainer.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import style from "./Friends.module.scss";
 import {connect} from "react-redux";
 import {getFriends} from "../../redux/selectors/SidebarSelectors";
 import Wrapper from "../common/Wrappers/WrapperComponents";
 
-console.log(matchMedia("screen and (min-width: 768px)"));
+const media = matchMedia("screen and (min-width: 768px)");
 
-const Friends = ({friendsBar}) => (
-    <Wrapper title="FRIENDS" className={style.main}>
-        <div className={style.main}>
-            {matchMedia("screen and (min-width: 768px)").matches && friendsBar.map(({name, image}) =>
-                <div key={name}>
-                    <img src={image} alt={name} width="50" height="50"/>
-                    <b>{name}</b>
-                </div>)}
-        </div>
-    </Wrapper>
-);
+const Friends = ({friendsBar}) => {
+    const [state, setState] = useState(() => media.matches);
+    useEffect(() => {
+        media.addEventListener("change", ({matches}) => {
+            console.log("start media");
+            setState(matches);
+        });
+        return () => media.removeEventListener("change", ({matches}) => {
+            setState(matches);
+        });
+        }, []);
+    return (
+        <Wrapper title="FRIENDS" className={style.main}>
+            <div className={style.main}>
+                <h2>sass:Math.div</h2>
+                {state && friendsBar.map(({name, image}) =>
+                    <div key={name}>
+                        <img src={image} alt={name} width="50" height="50"/>
+                        <b>{name}</b>
+                    </div>)}
+            </div>
+        </Wrapper>
+    );
+};
 
 const mapStateToProps = (state) => ({
     friendsBar: getFriends(state),
--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -3,46 +3,47 @@ import style from "./Music.module.css";
 import Wrapper from "../common/Wrappers/WrapperComponents";
 
 const eventLoop = () => {
-  setTimeout(() => {
-    console.log("setTimeout");
-  }, 0);
+    setTimeout(() => {
+        console.log("setTimeout");
+    }, 0);
 
-  function one() {
-    console.log("One");
-  }
+    function one() {
+        console.log("One");
+    }
 
-  const promise = new Promise(function two(resolve) {
-    console.log("Promise");
-    resolve("Promise.then");
-  });
+    const promise = new Promise(function two(resolve) {
+        console.log("Promise");
+        resolve("Promise.then");
+    });
 
-  one();
-  promise.then(res => console.log(res));
-  console.log("Simple");
-  queueMicrotask(() => console.log("queueMicrotask"));
+    one();
+    promise.then(res => console.log(res));
+    console.log("Simple");
+    queueMicrotask(() => console.log("queueMicrotask"));
 };
 
 const Music = () => {
-  const [state, setState] = useState(true);
+    const [state, setState] = useState(true);
 
-  useEffect(() => {
-    console.log("useEffect");
-  }, [state]);
+    useEffect(() => {
+        console.log("useEffect");
+    }, [state]);
 
-  useLayoutEffect(() => {
-    console.log("useLayoutEffect");
-  }, [state]);
+    useLayoutEffect(() => {
+        console.log("useLayoutEffect");
+    }, [state]);
 
-  const handleChange = () => {
-    eventLoop();
-    setState(value => !value);
-  };
-  return (
-    <Wrapper id="music" title="MUSIC" className={style.main}>
-      <button style={{padding: "30px"}} onClick={handleChange}>Event loop</button>
-      <strong style={{backgroundColor: "red", margin: "10px"}}>{state.toString()}</strong>
-    </Wrapper>
-  );
+    const handleChange = () => {
+        eventLoop();
+        setState(value => !value);
+    };
+    return (
+        <Wrapper id="music" title="MUSIC" className={style.main}>
+            <button style={{padding: "30px"}} onClick={handleChange}>Event loop</button>
+            <strong style={{backgroundColor: "red", margin: "10px"}}>{state.toString()}</strong>
+            <div className={style.image}/>
+        </Wrapper>
+    );
 };
 
 export default Music;
